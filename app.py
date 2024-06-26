8d23385c7d8ad6d0644b54352028d7ae4519a1e8
gregoryscott <gregory.scott99@imperial.ac.uk>
2024-06-26 14:10:17 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_data():
+    data = {
+        'name': 'Dhanush',
+        'age': 20,
+        'city': 'Hyderabad'
+    }
+    return jsonify(data)
